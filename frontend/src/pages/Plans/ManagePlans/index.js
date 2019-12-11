a65274dd5828289540b1ef7c60723f3f1d280c72
Stefano Saffran <stefanosaffran@hotmail.com>
2019-12-11 14:37:28 +0100

    feat: add yup validation to plans

--- frontend/src/pages/Plans/ManagePlans/index.js
+++ frontend/src/pages/Plans/ManagePlans/index.js
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import NumberFormat from 'react-number-format';
 import { MdKeyboardArrowLeft, MdSave } from 'react-icons/md';
+import * as Yup from 'yup';
+import { toast } from 'react-toastify';
 
 import { unFormat, formatPrice } from '~/helpers/format';
 import Loading from '~/components/Loading';
@@ -14,11 +16,22 @@ import {
   updatePlanRequest,
 } from '~/store/modules/plans/actions';
 
+const schema = Yup.object().shape({
+  title: Yup.string().required('Campo plano é obrigatório'),
+  duration: Yup.number()
+    .required('Campo duração é obrigatório')
+    .integer()
+    .positive('Duração precisa ser positivo'),
+  price: Yup.number()
+    .required('Campo preço é obrigatório')
+    .positive('Preço precisa ser positivo'),
+});
+
 export default function StorePlans() {
   const [title, setTitle] = useState('');
   const [duration, setDuration] = useState(0);
-  const [loading, setLoading] = useState(false);
   const [price, setPrice] = useState(0);
+  const [loading, setLoading] = useState(false);
   const { id } = useParams();
   const dispatch = useDispatch();
 
@@ -26,11 +39,11 @@ export default function StorePlans() {
     if (id) {
       setLoading(true);
       const getPlan = async () => {
-        const res = await api.get(`/plans/${id}`);
+        const { data } = await api.get(`/plans/${id}`);
 
-        setTitle(res.data.title);
-        setDuration(res.data.duration);
-        setPrice(res.data.price);
+        setTitle(data.title);
+        setDuration(data.duration);
+        setPrice(data.price);
         setLoading(false);
       };
 
@@ -43,9 +56,28 @@ export default function StorePlans() {
     price,
   ]);
 
-  const handleSubmit = e => {
-    setLoading(true);
+  const handleSubmit = async e => {
     e.preventDefault();
+
+    try {
+      await schema.validate(
+        {
+          title,
+          duration,
+          price: unFormat(price),
+        },
+        {
+          abortEarly: false,
+        }
+      );
+    } catch (err) {
+      err.inner.forEach((error: ValidationError) => {
+        toast.error(error.message);
+      });
+      return;
+    }
+
+    setLoading(true);
     if (id) {
       dispatch(updatePlanRequest(id, title, duration, unFormat(price)));
     } else {
