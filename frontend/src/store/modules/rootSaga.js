f0145516f6f2ebd25f87efc49b4aa358c0968c83
Stefano Saffran <stefanosaffran@hotmail.com>
2019-12-12 11:12:53 +0100

    chore: removing unused reducers and sagas

--- frontend/src/store/modules/rootSaga.js
+++ frontend/src/store/modules/rootSaga.js
@@ -2,9 +2,7 @@ import { all } from 'redux-saga/effects';
 
 import auth from './auth/sagas';
 import user from './user/sagas';
-import students from './students/sagas';
-import plans from './plans/sagas';
 
 export default function* rootSaga() {
-  return yield all([auth, user, students, plans]);
+  return yield all([auth, user]);
 }
