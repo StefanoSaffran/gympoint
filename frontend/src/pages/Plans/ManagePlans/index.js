2be91e627106d8bd7aa8c2b6c1530a2975d3c849
Stefano Saffran <stefanosaffran@hotmail.com>
2019-12-12 11:17:18 +0100

    chore: refactoring ManagePlans page accessibility / removing redux

--- frontend/src/pages/Plans/ManagePlans/index.js
+++ frontend/src/pages/Plans/ManagePlans/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import { useParams } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
 import NumberFormat from 'react-number-format';
 import { MdKeyboardArrowLeft, MdSave } from 'react-icons/md';
 import * as Yup from 'yup';
@@ -11,10 +10,6 @@ import Loading from '~/components/Loading';
 import history from '~/services/history';
 import api from '~/services/api';
 import { Container, Header } from './styles';
-import {
-  storePlanRequest,
-  updatePlanRequest,
-} from '~/store/modules/plans/actions';
 
 const schema = Yup.object().shape({
   title: Yup.string().required('Campo plano é obrigatório'),
@@ -33,7 +28,6 @@ export default function StorePlans() {
   const [price, setPrice] = useState(0);
   const [loading, setLoading] = useState(false);
   const { id } = useParams();
-  const dispatch = useDispatch();
 
   useEffect(() => {
     if (id) {
@@ -43,7 +37,7 @@ export default function StorePlans() {
 
         setTitle(data.title);
         setDuration(data.duration);
-        setPrice(data.price);
+        setPrice(formatPrice(data.price));
         setLoading(false);
       };
 
@@ -51,10 +45,9 @@ export default function StorePlans() {
     }
   }, [id]);
 
-  const total = useMemo(() => formatPrice(unFormat(price) * duration), [
-    duration,
-    price,
-  ]);
+  const total = useMemo(() => {
+    return formatPrice(unFormat(price) * duration);
+  }, [duration, price]);
 
   const handleSubmit = async e => {
     e.preventDefault();
@@ -78,12 +71,35 @@ export default function StorePlans() {
     }
 
     setLoading(true);
-    if (id) {
-      dispatch(updatePlanRequest(id, title, duration, unFormat(price)));
-    } else {
-      dispatch(storePlanRequest(title, duration, unFormat(price)));
+
+    try {
+      if (id) {
+        await api.put(`plans/${id}`, {
+          title,
+          duration,
+          price: unFormat(price),
+        });
+
+        toast.success('Plano atualizado com sucesso');
+        history.push('/plans');
+      } else {
+        const { data } = await api.post('plans', {
+          title,
+          duration,
+          price: unFormat(price),
+        });
+
+        toast.success('Plano cadastrado com sucesso');
+        history.push(`/plans/${data.id}`);
+      }
+    } catch (err) {
+      toast.error(
+        (err.response && err.response.data.error) ||
+          'Erro de comunicação com o servidor'
+      );
+    } finally {
+      setLoading(false);
     }
-    setLoading(false);
   };
 
   return (
@@ -107,7 +123,7 @@ export default function StorePlans() {
           </Header>
           <form id="form-plans" onSubmit={handleSubmit}>
             <div>
-              <span>TÍTULO DO PLANO</span>
+              <label>TÍTULO DO PLANO</label>
               <input
                 id="title"
                 name="title"
@@ -118,7 +134,7 @@ export default function StorePlans() {
               />
             </div>
             <div>
-              <span>
+              <label>
                 DURAÇÃO (em meses)
                 <input
                   id="duration"
@@ -128,8 +144,8 @@ export default function StorePlans() {
                   value={duration || ''}
                   onChange={({ target }) => setDuration(target.value)}
                 />
-              </span>
-              <span>
+              </label>
+              <label>
                 PREÇO MENSAL
                 <NumberFormat
                   thousandSeparator="."
@@ -140,19 +156,11 @@ export default function StorePlans() {
                   value={price || ''}
                   onChange={({ target }) => setPrice(target.value)}
                 />
-              </span>
-              <span>
+              </label>
+              <label>
                 PREÇO TOTAL
-                <NumberFormat
-                  thousandSeparator="."
-                  decimalSeparator=","
-                  fixedDecimalScale={2}
-                  prefix="R$ "
-                  name="total"
-                  value={total || ''}
-                  disabled
-                />
-              </span>
+                <input type="text" name="total" value={total || ''} disabled />
+              </label>
             </div>
           </form>
         </>
