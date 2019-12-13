feebfc41a9504cda9e9fae9dcc0ebe85cf866dce
Stefano Saffran <stefanosaffran@hotmail.com>
2019-12-13 23:22:06 +0100

    feat: Implementing socketio in the frontend

--- frontend/src/pages/HelpOrders/index.js
+++ frontend/src/pages/HelpOrders/index.js
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 
+import { useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
 import { FaClipboardCheck } from 'react-icons/fa';
+import socketio from 'socket.io-client';
 
 import Loading from '~/components/Loading';
 import Modal from '~/components/Modal';
@@ -10,9 +12,29 @@ import api from '~/services/api';
 import { Container, EmptyList, HelpOrdersList } from './styles';
 
 export default function HelpOrders() {
-  const [orders, setOrders] = useState(null);
+  const [orders, setOrders] = useState([]);
   const [order, setOrder] = useState(null);
   const [loading, setLoading] = useState(true);
+
+  const user = useSelector(state => state.user.profile);
+
+  const socket = useMemo(
+    () =>
+      socketio('http://localhost:3003', {
+        query: {
+          user_id: `ADMIN-${user.id}`,
+          origin: 'web',
+        },
+      }),
+    [user.id]
+  );
+
+  useEffect(() => {
+    socket.on('new_order', newOrder => {
+      console.tron.log(orders, newOrder);
+      setOrders([...orders, newOrder]);
+    });
+  }, [orders, socket]);
 
   const modalRef = React.createRef();
 
