66d5b384d157740d013aba388ee11dfb0c0f2ebf
Stefano Saffran <stefanosaffran@hotmail.com>
2019-12-16 14:30:57 +0100

    feat: pagination for checkins - infinite scroll

--- mobile/src/pages/CheckInsPage/Checkins/index.js
+++ mobile/src/pages/CheckInsPage/Checkins/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Alert } from 'react-native';
+import { Alert, ActivityIndicator } from 'react-native';
 import { withNavigationFocus } from 'react-navigation';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
@@ -20,11 +20,18 @@ import {
 
 function Checkins({ isFocused }) {
   const [checkins, setCheckins] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [totalCheckins, setTotalCheckins] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [loadingMore, setLoadingMore] = useState(false);
   const id = useSelector(state => state.student.profile.student.id);
 
   const loadCheckins = async () => {
-    const { data } = await api.get(`students/${id}/checkins`);
-    console.tron.log(data);
+    const { data } = await api.get(`students/${id}/checkins`, {
+      params: { page },
+    });
+
     const formattedData = data.checkins.map(checkin => ({
       ...checkin,
       timeDistance: formatDistance(parseISO(checkin.createdAt), new Date(), {
@@ -33,22 +40,40 @@ function Checkins({ isFocused }) {
       }),
     }));
 
-    setCheckins(formattedData);
+    setCheckins(page > 1 ? [...checkins, ...formattedData] : formattedData);
+    setTotalPages(Math.ceil(data.count / 10));
+    setTotalCheckins(data.count);
+    setLoading(false);
+    setLoadingMore(false);
+  };
+
+  const loadMore = () => {
+    if (page === totalPages) return;
+
+    setLoadingMore(true);
+    setPage(page + 1);
   };
 
   useEffect(() => {
     if (isFocused) {
       loadCheckins();
     }
-  }, [isFocused]); // eslint-disable-line
+  }, [isFocused, page]); // eslint-disable-line
 
   const handleCheckin = async () => {
     try {
       await api.post(`students/${id}/checkins`);
 
-      loadCheckins();
+      if (page === 1) return loadCheckins();
+
+      setPage(1);
     } catch (err) {
-      Alert.alert('Falha ao realizar o CheckIn', err.response.data.error);
+      Alert.alert(
+        'Falha ao realizar o CheckIn',
+        err.response
+          ? err.response.data.error
+          : 'Erro de conexão com o servidor'
+      );
     }
   };
 
@@ -56,16 +81,23 @@ function Checkins({ isFocused }) {
     <Background>
       <Container>
         <CheckinButton onPress={handleCheckin}>Novo check-in</CheckinButton>
-        <CheckinList
-          data={checkins.sort((a, b) => b.id - a.id)}
-          keyExtractor={item => String(item.id)}
-          renderItem={({ item }) => (
-            <Checkin>
-              <Left>{`Check-in #${item.id}`}</Left>
-              <Right>{item.timeDistance}</Right>
-            </Checkin>
-          )}
-        />
+        {loading ? (
+          <ActivityIndicator color="#ee4e62" size={30} />
+        ) : (
+          <CheckinList
+            data={checkins.sort((a, b) => b.id - a.id)}
+            keyExtractor={item => String(item.id)}
+            renderItem={({ item, index }) => (
+              <Checkin>
+                <Left>{`Check-in #${totalCheckins - index}`}</Left>
+                <Right>{item.timeDistance}</Right>
+              </Checkin>
+            )}
+            onEndReachedThreshold={0.2}
+            onEndReached={loadMore}
+          />
+        )}
+        {loadingMore && <ActivityIndicator color="#ee4e62" size={30} />}
       </Container>
     </Background>
   );
