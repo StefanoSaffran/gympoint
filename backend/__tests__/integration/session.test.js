b72afad9f380893e0ca85b68c6a2079f2202b83c
Stefano Saffran <stefanosaffran@hotmail.com>
2019-12-20 12:23:01 +0100

    feat: finish the integration tests for SessionController

--- backend/__tests__/integration/session.test.js
+++ backend/__tests__/integration/session.test.js
@@ -3,6 +3,7 @@ import app from '../../src/app';
 
 import factory from '../factories';
 import truncate from '../util/truncate';
+import getToken from '../getToken';
 
 describe('Authentication', () => {
   beforeEach(async () => {
@@ -10,6 +11,12 @@ describe('Authentication', () => {
   });
 
   it('should authenticate with valid credentials', async () => {
+    const response = await getToken();
+
+    expect(response.status).toBe(200);
+  });
+
+  it('should not authenticate with invalid credentials', async () => {
     const user = await factory.create('User', {
       password: '123456',
     });
@@ -18,9 +25,68 @@ describe('Authentication', () => {
       .post('/sessions')
       .send({
         email: user.email,
+        password: '123123',
+      });
+
+    expect(response.status).toBe(401);
+  });
+
+  it('should return jwt token when authenticated', async () => {
+    const { body } = await getToken();
+
+    expect(body).toHaveProperty('token');
+  });
+
+  it('should be able to access private routes when authenticated', async () => {
+    const { body } = await getToken();
+
+    const { status } = await request(app)
+      .get('/students')
+      .set('Authorization', `Bearer ${body.token}`);
+
+    expect(status).toBe(200);
+  });
+
+  it('should not be able to access private routes without jwt token', async () => {
+    const response = await request(app).get('/students');
+
+    expect(response.status).toBe(401);
+  });
+
+  it('should not be able to access private routes with invalid jwt token', async () => {
+    const response = await request(app)
+      .get('/students')
+      .set('Authorization', `Bearer 123456`);
+
+    expect(response.status).toBe(401);
+  });
+
+  it('should not be able to access when email or password is not provided', async () => {
+    const user = await factory.create('User', {
+      password: '123456',
+    });
+
+    const response = await request(app)
+      .post('/sessions')
+      .send({
         password: user.password,
       });
 
-    expect(response.status).toBe(200);
+    expect(response.status).toBe(400);
+  });
+
+  it('should not be able to access when user does not exist', async () => {
+    const user = await factory.create('User', {
+      password: '123456',
+    });
+
+    const response = await request(app)
+      .post('/sessions')
+      .send({
+        email: 'anotheremail@email.com',
+        password: user.password,
+      });
+
+    expect(response.status).toBe(401);
   });
 });
